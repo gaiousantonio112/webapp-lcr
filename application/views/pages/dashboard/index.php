d3dafc2c6efb8b448f548f5472104f13c54392f2
hijax231 <jandeanpasajol231@gmail.com>
2019-06-13 11:52:25 +0800

    pdf viewer/modal

--- application/views/pages/dashboard/index.php
+++ application/views/pages/dashboard/index.php
@@ -1,5 +1,38 @@
 <div class="container p-4">
   <div class="container">
 
-  
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#centralModalSm">
+      Launch demo modal
+    </button>
+
+    <div class="modal fade" id="centralModalSm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+      aria-hidden="true">
+
+      <!-- Change class .modal-sm to change the size of the modal -->
+      <div class="modal-dialog modal-lg" role="document">
+
+
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title w-100" id="myModalLabel">PDF</h4>
+
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+
+                <embed src="../../../../assets/pdf/pdftron-sdk.pdf#toolbar=0&zoom=50" type="application/pdf" width="100%" height="60%">
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary btn-sm">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Central Modal Small -->
+
+
+
 </div>
