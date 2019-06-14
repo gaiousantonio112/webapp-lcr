e5e26fa9410a253f2b3731a2700cc646df445d51
hijax231 <jandeanpasajol231@gmail.com>
2019-06-14 15:29:19 +0800

    loop for pdrf merge

--- application/views/pages/dashboard/pdfmerge.php
+++ application/views/pages/dashboard/pdfmerge.php
@@ -12,6 +12,8 @@ $pdf->setSourceFile(APPPATH.'/..//assets/pdf/1.pdf');
  $tpl = $pdf->importPage(1);
 $size = $pdf->getTemplateSize($tpl);
  $orientation = $size['h'] > $size['w'] ? 'P' : 'L';
+ $pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
 $pdf->addPage($orientation);
  $pdf->useTemplate($tpl, null, null, 0, 0, TRUE);
 
@@ -30,6 +32,34 @@ $pdf->addPage($orientation);
   $pdf->Text(0, 0, 'JAN DEAN M. PASAJOL JAN DEAN M. PASAJOL JAN DEAN M. PASAJOL JAN DEAN M. PASAJOL JAN DEAN M. PASAJOL');
   // Stop Transformation
   $pdf->StopTransform();
+
+
+// loopdis bebe
+
+  $tpl = $pdf->importPage(2);
+
+ $size = $pdf->getTemplateSize($tpl);
+  $orientation = $size['h'] > $size['w'] ? 'P' : 'L';
+  $pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+ $pdf->addPage($orientation);
+  $pdf->useTemplate($tpl, null, null, 0, 0, TRUE);
+
+
+  $pdf->StartTransform();
+  // Rotate 20 degrees counter-clockwise centered by (70,110) which is the lower left corner of the rectangle
+  $pdf->Rotate(90, 25, 240);
+  $pdf->Text(0, 220, 'JAN DEAN M. PASAJOL JAN DEAN M. PASAJOL JAN DEAN M. PASAJOL JAN DEAN M. PASAJOL JAN DEAN M. PASAJOL');
+  // Stop Transformation
+  $pdf->StopTransform();
+
+
+   $pdf->StartTransform();
+   // Rotate 20 degrees counter-clockwise centered by (70,110) which is the lower left corner of the rectangle
+   $pdf->Rotate(-90, 90, 120);
+   $pdf->Text(0, 0, 'JAN DEAN M. PASAJOL JAN DEAN M. PASAJOL JAN DEAN M. PASAJOL JAN DEAN M. PASAJOL JAN DEAN M. PASAJOL');
+   // Stop Transformation
+   $pdf->StopTransform();
 
 $pdf->Output('name.pdf', 'I');
 
