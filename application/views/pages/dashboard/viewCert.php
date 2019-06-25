5667a69a51bec1ecd704d5ebe30e16b30698d641
gaiousantonio112 <gaiousantonio4@gmail.com>
2019-06-25 13:26:33 +0800

    pdf Chngers

--- application/views/pages/dashboard/viewCert.php
+++ application/views/pages/dashboard/viewCert.php
@@ -11,8 +11,18 @@ $date = date('m - d - Y');
 
   $pdfv2 = new TCPDI(PDF_PAGE_ORIENTATION, 'mm', PDF_PAGE_FORMAT, true, 'UTF-8', false);
   // '\\\\192.168.100.164\\lcr\\birthday\\'
-  $pdfv2->setSourceFile('\\\\192.168.100.164\\lcr\\'.$_GET['type'].'\\'.$_GET['ref'].'.pdf');
-    $tpl = $pdfv2->importPage(1);
+  $pageCount = $pdfv2->setSourceFile('\\\\192.168.100.164\\lcr\\'.$_GET['type'].'\\'.$_GET['ref'].'.pdf');
+
+
+  // 
+  //  $pdfv2->addpage();
+  //  //$pdfv2->deletePage(1);
+  // // $pdfv2->Text(55,50 , $pageCount);
+
+$pageNo = 1;
+
+  for ($pageNo; $pageNo <= $pageCount; $pageNo++) {
+    $tpl = $pdfv2->importPage($pageNo);
   $size = $pdfv2->getTemplateSize($tpl);
    $orientation = $size['h'] > $size['w'] ? 'P' : 'L';
    $pdfv2->setPrintHeader(false);
@@ -20,11 +30,8 @@ $date = date('m - d - Y');
   $pdfv2->addPage($orientation);
    $pdfv2->useTemplate($tpl, null, null, 0, 0, TRUE);
 
+  }
 
-
-   $pdfv2->addpage();
-   //$pdfv2->deletePage(1);
-  //$pdfv2->Text(55,50 , $date);
   //$pdfv2->Text(17,65 , ($_GET['payor'] == null ? 'no payor input!' : $_GET['payor']));
   //$pdfv2->Text(10,83 , ($_GET['what'] == null ? '0' : $_GET['what']));
 
