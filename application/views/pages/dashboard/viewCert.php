db473d2895168c0bb54113ef441fe97c6771d8e4
hijax231 <jandeanpasajol231@gmail.com>
2019-07-25 11:51:02 +0800

    genform post search file not exist

--- application/views/pages/dashboard/viewCert.php
+++ application/views/pages/dashboard/viewCert.php
@@ -7,27 +7,38 @@ require_once(APPPATH.'/../assets/pdfmerge/tcpdi/tcpdi.php');
 date_default_timezone_set('Asia/Manila');
 $date = date('m - d - Y');
 
+$file_pointer = '\\\\192.168.100.164\\lcr\\'.$_GET['type'].'\\'.$_GET['ref'].'.pdf';
+
+        if (file_exists($file_pointer)) {
+
+          $pdfv2 = new TCPDI(PDF_PAGE_ORIENTATION, 'mm', PDF_PAGE_FORMAT, true, 'UTF-8', false);
+          $pageCount = $pdfv2->setSourceFile('\\\\192.168.100.164\\lcr\\'.$_GET['type'].'\\'.$_GET['ref'].'.pdf');
+
+        $pageNo = 1;
+
+          for ($pageNo; $pageNo <= $pageCount; $pageNo++) {
+              $pdfv2->SetDisplayMode(100);
+            $tpl = $pdfv2->importPage($pageNo);
+          $size = $pdfv2->getTemplateSize($tpl);
+           $orientation = $size['h'] > $size['w'] ? 'P' : 'L';
+           $pdfv2->setPrintHeader(false);
+          $pdfv2->setPrintFooter(false);
+          $pdfv2->addPage($orientation);
+           $pdfv2->useTemplate($tpl, null, null, 0, 0, TRUE);
 
 
-  $pdfv2 = new TCPDI(PDF_PAGE_ORIENTATION, 'mm', PDF_PAGE_FORMAT, true, 'UTF-8', false);
-  $pageCount = $pdfv2->setSourceFile('\\\\192.168.100.164\\lcr\\'.$_GET['type'].'\\'.$_GET['ref'].'.pdf');
-
-$pageNo = 1;
-
-  for ($pageNo; $pageNo <= $pageCount; $pageNo++) {
-      $pdfv2->SetDisplayMode(100);
-    $tpl = $pdfv2->importPage($pageNo);
-  $size = $pdfv2->getTemplateSize($tpl);
-   $orientation = $size['h'] > $size['w'] ? 'P' : 'L';
-   $pdfv2->setPrintHeader(false);
-  $pdfv2->setPrintFooter(false);
-  $pdfv2->addPage($orientation);
-   $pdfv2->useTemplate($tpl, null, null, 0, 0, TRUE);
-
-  }
-  ob_end_clean();
+         }
+         ob_end_clean();
 
 
-   $pdfv2->Output('print.pdf', 'I');
+
+            $pdfv2->Output('print.pdf', 'I');
+
+
+        }else {
+            echo "The file  does
+                                   not exists";
+        }
+
 
 ?>
