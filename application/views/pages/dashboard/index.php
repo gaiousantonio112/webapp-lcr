09327facd45867eb8796e1e8897198d0f36f0fae
jovelle19 <jovelle.espinili@gmail.com>
2019-07-29 17:28:50 -0700

    dashboard upd

--- application/views/pages/dashboard/index.php
+++ application/views/pages/dashboard/index.php
@@ -4,106 +4,109 @@
 	<div class=" container text-right pt-3" >
 					<div class="teal lighten-1 z-depth-1 " style="height:52px;border-radius:3px">
 						<span><img src="<?php echo base_url() .'/assets/svg/dashboard2(samp).svg' ?>" height="40px" alt="" style="float:right" class="px-3 mt-2"></span>
-						<p style="color:white; float:right; font-size: 30px;" class="pt-2">Dashboard</p>
+							<p class="text-title">Dashboard</p>
 					</div>
 	</div>
 
-	<div class="container py-5 pt-4">
+	<div class="container py-5 pt-1">
 		<div class="row">
-
-			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 ">
-				<div class="card mt-4">
-					<div class="card-body card-dashboard">
-						<div class = "container ">
-						<div class = "row">
-						<div class = "col-5 p-0">
-							<div class="card card-icon warning-color-dark">
-										<img src="<?php echo base_url() .'/assets/svg/service provided2(samp).svg' ?>" height="45px" alt="">
-							</div>
-						</div>
-							<div class = "col-7 ">
-								<div class="text-count " id="service_provided">Loading...</div>
-							</div>
-						</div>
-					</div>
-					<hr>
-					<div class="text-dashboard ">
-						Service Provided</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 ">
-				<div class="card mt-4">
-					<div class="card-body card-dashboard">
-						<div class = "container">
-						<div class = "row">
-						<div class = "col-5 p-0">
-							<div class="card card-icon info-color-dark">
-										<img src="<?php echo base_url() .'/assets/svg/earnings 2(samp).svg' ?>" height="45px" alt="">
-							</div>
-						</div>
-							<div class = "col-7 ">
-								<div class="text-count " id="total_earnings">Loading...</div>
-							</div>
-						</div>
-					</div>
-					<hr>
-					<div class="text-dashboard ">
-					Total Earnings</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 ">
-				<div class="card mt-4">
-					<div class="card-body card-dashboard">
-						<div class = "container">
-						<div class = "row">
-						<div class = "col-5 p-0">
-							<div class="card card-icon cyan">
-										<img src="<?php echo base_url() .'/assets/svg/task2(samp).svg' ?>" height="45px" alt="">
-							</div>
-						</div>
-							<div class = "col-7 ">
-								<div class="text-count " id="task_text_percent">Loading...</div>
-							</div>
-						</div>
-					</div>
-
-					<div class="progress progress-sm " style="height:5px">
-						<div class="progress-bar bg-info" role="progressbar" id="task_bar_percent" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-					</div>
-					<div class="text-dashboard mt-2">
-					Tasks</div>
-					</div>
-				</div>
-			</div>
-
-
-			<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 ">
-				<div class="card mt-4">
-					<div class="card-body card-dashboard">
-						<div class = "container">
-						<div class = "row">
-						<div class = "col-5 p-0">
-							<div class="card card-icon default-color">
-										<img src="<?php echo base_url() .'/assets/svg/pending request2(samp).svg' ?>" height="45px" alt="">
-							</div>
-						</div>
-							<div class = "col-7 ">
-								<div class="text-count " id="pending_request">Loading...</div>
-							</div>
-						</div>
-					</div>
-					<hr>
-					<div class="text-dashboard ">
-					Pending Requests</div>
-					</div>
-				</div>
-			</div>
-
 	</div>
+</div>
+
+
+<div class = "container">
+	<div class = "row">
+
+		<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 ">
+			<div class="card mt-4">
+				<div class="card-body card-dashboard">
+					<div class = "container ">
+					<div class = "row">
+					<div class = "col-5 p-0">
+						<div class="card card-icon warning-color-dark">
+									<img src="<?php echo base_url() .'/assets/svg/service provided2(samp).svg' ?>" height="45px" alt="">
+						</div>
+					</div>
+						<div class = "col-7 ">
+							<div class="text-count " id="service_provided">Loading...</div>
+						</div>
+					</div>
+				</div>
+				<hr>
+				<div class="text-dashboard ">Service Provided</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 ">
+			<div class="card mt-4">
+				<div class="card-body card-dashboard">
+					<div class = "container">
+					<div class = "row">
+					<div class = "col-5 p-0">
+						<div class="card card-icon info-color-dark">
+									<img src="<?php echo base_url() .'/assets/svg/earnings 2(samp).svg' ?>" height="45px" alt="">
+						</div>
+					</div>
+						<div class = "col-7 ">
+							<div class="text-count " id="total_earnings">Loading...</div>
+						</div>
+					</div>
+				</div>
+				<hr>
+				<div class="text-dashboard ">Total Earnings</div>
+				</div>
+			</div>
+		</div>
+
+					<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 ">
+						<div class="card mt-4">
+							<div class="card-body card-dashboard">
+								<div class = "container">
+								<div class = "row">
+								<div class = "col-5 p-0">
+									<div class="card card-icon cyan">
+												<img src="<?php echo base_url() .'/assets/svg/task2(samp).svg' ?>" height="45px" alt="">
+									</div>
+								</div>
+									<div class = "col-7 ">
+										<div class="text-count " id="task_text_percent">Loading...</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="progress progress-sm " style="height:5px">
+								<div class="progress-bar bg-info" role="progressbar" id="task_bar_percent" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+							<div class="text-dashboard mt-2">
+							Tasks</div>
+							</div>
+						</div>
+					</div>
+
+
+		<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 ">
+			<div class="card mt-4">
+				<div class="card-body card-dashboard">
+					<div class = "container">
+					<div class = "row">
+					<div class = "col-5 p-0">
+						<div class="card card-icon default-color">
+									<img src="<?php echo base_url() .'/assets/svg/pending request2(samp).svg' ?>" height="45px" alt="">
+						</div>
+					</div>
+						<div class = "col-7 ">
+							<div class="text-count " id="pending_request">Loading...</div>
+						</div>
+					</div>
+				</div>
+				<hr>
+				<div class="text-dashboard ">
+				Pending Requests</div>
+				</div>
+			</div>
+		</div>
+</div>
 </div>
 
 <div class = "container">
