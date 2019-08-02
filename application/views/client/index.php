5b0f6e5165e36fb5a5538524b9a6c6632d8e61d0
jovelle19 <jovelle.espinili@gmail.com>
2019-08-01 20:13:45 -0700

    clin

---
+++ application/views/client/index.php
@@ -0,0 +1,32 @@
+<div class="container-fluid pt-4 px-5 "  >
+
+			<div class="z-depth-1 title-Div">
+				<span><img class = "title-Divimg" src="<?php echo base_url() .'/assets/svg/add2(ecris).svg' ?>" ></span>
+				<h2 class="white-text float-right title-text"> Client </h2>
+			</div>
+
+
+<div class="card mt-3">
+  <div class ="card-body">
+	<div class="container p-4" >
+  	<div class="row">
+
+      <hr>
+			<div class="col-4">
+            <!-- <img class = "title-Divimg" src="<?php echo base_url() .'/assets/svg/add2(ecris).svg' ?>" > -->
+						<label class="" for="Birthday">Birthday</label>
+			</div>
+			<div class="col-4">
+            <!-- <img class = "title-Divimg" src="<?php echo base_url() .'/assets/svg/add2(ecris).svg' ?>" > -->
+						<label class="" for="Death">Death</label>
+			</div>
+			<div class="col-4">
+            <!-- <img class = "title-Divimg" src="<?php echo base_url() .'/assets/svg/add2(ecris).svg' ?>" > -->
+						<label class="" for="Marriage">Marriage</label>
+			</div>
+		</div>
+
+    </div>
+    </div>
+  </div>
+</div>
---
+++ application/views/client/nav.php
@@ -0,0 +1,127 @@
+<link rel="stylesheet" href="../../../../assets/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="../../../../assets/bootstrap/css/bootstrap-grid.css">
+<link rel="stylesheet" href="../../../../assets/fontawesome-5.9.0/css/all.min.css">
+<link rel="stylesheet" href="../../../../assets/css/sideslide.css">
+<link rel="stylesheet" href="../../../../assets/css/simple-sidebar0.css">
+<link rel="stylesheet" href="../../../../assets/MBD/css/mdb.min.css">
+<link rel="stylesheet" href="../../../../assets/MBD/css/style.css">
+<link rel="stylesheet" href="../../../../assets/MBD/css/modules/animations-extended.min.css">
+<link rel="stylesheet" href="../../../../assets/MBD/css/addons/datatables.min.css">
+<link rel="stylesheet" href="../../../../assets/MBD/css/addons/datatables-select.min.css">
+<link rel="stylesheet" href="../../../../assets/datepicker/calendar/lib/bootstrap-datepicker.css">
+<link rel="stylesheet" href="../../../../assets/datepicker/calendar/lib/daterangepicker.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>/assets/css/pages.css" >
+<style media="screen">
+
+
+.wrap
+{
+    width: ;
+    height: ;
+    padding: 0;
+    overflow: hidden;
+}
+
+.bg-ecris{
+background-color: #EFEDDF;
+}
+
+.bg-title{
+  color: #163F54;
+}
+.notif:hover
+{
+    background-color:#F3F3F3;
+}
+.notif
+{
+    background-color: white;
+    padding: 10px 0px;
+}
+.notif:active
+{
+    background-color: #EFECE0;
+}
+.curr-name
+{
+  overflow-wrap: break-word;
+      /* font-size: calc(12px + (26 - 14) * ((100vw - 300px) / (1600 - 300))); */
+    margin:0;
+}
+
+</style>
+
+<!DOCTYPE html>
+<html lang="en" class="">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>LCR
+
+  </title>
+</head>
+<body>
+
+<div class="d-flex wrapper" id="wrapper" >
+  <!-- Sidebar -->
+<nav class="bg-white rounded-bottom z-depth-1">
+
+  
+</nav>
+    <!-- /#sidebar-wrapper -->
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+      <nav class="navbar navbar fixed-top navbar-expand-lg navbar-light bg-white z-depth-1 py-0 " style="border-bottom:3px solid #3F729B ;">
+
+       <div class = "mt-2">
+        <p class="electext px-0">ELECTRONIC CIVIL REGISTRY INFORMATION SYSTEM</p>
+      </div>
+
+         <button class="navbar-toggler"  type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon" style="color:teal"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+          <ul class="navbar-nav ml-auto mt-2  mt-lg-0" >
+            <div class ="px-2">
+
+            <li class="nav-item dropdown ">
+                <span class="badge badge-default float-right px-2" id="notif_num">Loading...</span>
+                <a class="nav-link dropdown-toggle bg-title" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="far fa-bell pb-2" style="color:teal"></i>
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-right pb-0 "  id="notif" aria-labelledby="navbarDropdown" style="background-color:tomato;">
+                  <h6 class="dropdown-header mt-0 pb-3 white-text" style="background-color:tomato; width:230px;">Notification   <i class="fas fa-bell float-right px-0"></i></h6>
+                  <div class="" style="height: 300px; overflow-y:scroll" id="notif_load">
+
+                  </div>
+                </div>
+            </li>
+          </div>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
+                <i class="far fa-user-circle fa-lg pr-1" style="color:teal"></i>
+                <span id="curruser_name">
+
+                </span>
+              </a>
+
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                <a class="dropdown-item" href="<?php echo base_uSrl() . 'pages/LCR'  ?>">Logout</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+  </nav>
+
+<div class = "m-5"></div>
+
+
+    <!-- /#page-content-wrapper -->
